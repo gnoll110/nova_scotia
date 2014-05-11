c391c0ac493fc63c730328b04e0add86406648b2
Noel Kelly <gnoll110@yahoo.com.au>
2014-05-11 10:14:31 +1000

    mod t/j/test_hepler.js - fix style related writes

--- test/javascripts/test_helper.js
+++ test/javascripts/test_helper.js
@@ -2,9 +2,9 @@
 //= require_tree .
 //= require_self
 
-document.write(' id="ember-testing-container"><div id="ember-testing"></div></div>');
+document.write('<div id="ember-testing-container"><div id="ember-testing"></div></div>');
 
-document.write(' { position: absolute; background: white; bottom: 0; right: 0; width: 640px; height: 384px; overflow: auto; z-index: 9999; border: 1px solid #ccc; } #ember-testing { zoom: 50%; }</style>');
+document.write('<style> #ember-testing-container { position: absolute; background: white; bottom: 0; right: 0; width: 640px; height: 384px; overflow: auto; z-index: 9999; border: 1px solid #ccc; } #ember-testing { zoom: 50%; }</style>');
 
 App.rootElement = '#ember-testing';
 App.setupForTesting();
